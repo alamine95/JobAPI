889b4be86d23140f271496d9fa188b6d46e762d7
alamine95 <lsow313@gmail.com>
2022-04-10 16:18:10 +0000

    password compare

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,10 +15,14 @@ const login = async (req, res) => {
         throw new BadRequestError('Please provide email and password')
     }
     const user = await User.findOne({email})
-    // Compare password
     if (!user){
         throw new UnauthenticatedError('Invalid Credentials')
     }
+    const isPasswordCorrect = await user.comparePassword(password)
+    if (!isPasswordCorrect){
+        throw new UnauthenticatedError('Invalid Credentials')
+    }
+    // Compare password
     const token = user.createJWT()
     res.status(StatusCodes.OK).json({ user: { name: user.name }, token })
 }
