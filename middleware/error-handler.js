55a92ac739615beba792c57b2f6459eb4a00d3c6
alamine95 <lsow313@gmail.com>
2022-04-16 19:21:12 +0000

    error handler

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -10,12 +10,20 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   // if (err instanceof CustomAPIError) {
   //   return res.status(err.statusCode).json({ msg: err.message })
   // }
+
+  if (err.name === 'ValidationError') {
+    custumError.msg = Object.values(err.errors).map((item) => item.message).join(',')
+    custumError.statusCode = 400
+  }
   if (err.code && err.code === 11000){
     custumError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose
     another value`
     custumError.statusCode = 400
   }
-  //return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  if (err.name === 'CastError') {
+    custumError.msg = `No item found with id : ${err.value}`
+    custumError.statusCode = 404
+  }
   return res.status(custumError.statusCode).json({ msg: custumError.msg })
 }
 
